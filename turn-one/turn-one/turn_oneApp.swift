678b91046e7737dc6f2244b09399c77b6306c7be
aeremic <andrija.eremic@gmail.com>
2024-08-19 21:34:38 +0200

    init

---
+++ turn-one/turn-one/turn_oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  turn_oneApp.swift
+//  turn-one
+//
+//  Created by Andrija Eremić on 19.8.24..
+//
+
+import SwiftUI
+
+@main
+struct turn_oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
